8011c6f62e1a992e26a6f043aab7bde2c95ea73e
Robin cabrel Harper <cabrel@zerklabs.com>
2014-11-05 13:33:14 -0500

    Added godep

--- libsmtp.go
+++ libsmtp.go
@@ -13,7 +13,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/zerklabs/auburn/utils"
+	"github.com/zerklabs/libsmtp/Godeps/_workspace/src/github.com/zerklabs/auburn/utils"
 )
 
 type MailMessage struct {
